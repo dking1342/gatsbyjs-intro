d7642e3784850dc068af7e0528f40b98d458b1d5
David <leanmachine42@yahoo.com>
2022-06-29 00:35:08 -0500

    static queries in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 import React from 'react'
 
 const Navbar = () => {
+  const data = useStaticQuery(graphql`
+    query SiteMetaData{
+      site { 
+        siteMetadata{
+          title
+        }
+      }
+    }
+  `)
+
+  const { title } = data.site.siteMetadata;
+
   return (
     <nav>
-      <h1><Link to="/"><strong>Web Warrior</strong></Link></h1>
+      <h1><Link to="/"><strong>{ title }</strong></Link></h1>
       <div className="links">
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>

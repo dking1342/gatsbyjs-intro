1b6600ffa6c82ff97cc9e5fb614b2318a8e6ff46
David <leanmachine42@yahoo.com>
2022-06-28 22:38:02 -0500

    made layout component and included in each component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Navbar />
+      <main className="content">
+        { children }
+      </main>
+      <footer>
+        <p>Copyright &copy; 2022 Web Warrior</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
--- src/components/layoutDemo.js
+++ src/components/layoutDemo.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
@@ -26,7 +19,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Navbar />
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
